c7e522e88e245893bed25d9293e16a682c098822
olegz-codefresh <olegz@codefresh.io>
2020-08-19 15:07:25 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 http.createServer(function (req, res) {
-    res.write('Hello World!22123231111');
+    res.write('Hello World!22123vv231111');
     res.end();
 }).listen(1700);
