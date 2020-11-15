24b933ab1f246b344c3e586e5a055572acf63c06
pasha-codefresh <pavel@codefresh.io>
2020-11-15 23:38:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 const http = require('http');
 
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
-sleep(30000);
-
 http.createServer(function (req, res) {
     res.write('Hello World!');
     res.end();
